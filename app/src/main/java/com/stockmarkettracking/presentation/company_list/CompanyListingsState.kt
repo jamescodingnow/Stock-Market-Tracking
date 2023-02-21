42404e7a387ccccbebd1e2a1ed1d131f8f925c42
jamescodingnow <114888767+jamescodingnow@users.noreply.github.com>
2023-02-20 22:45:09 -0500

    List ViewModel State Event Classes created.
    ViewModel State Event Classes created for List page.

---
+++ app/src/main/java/com/stockmarkettracking/presentation/company_list/CompanyListingsState.kt
@@ -0,0 +1,10 @@
+package com.stockmarkettracking.presentation.company_list
+
+import com.stockmarkettracking.domain.model.CompanyListing
+
+data class CompanyListingsState(
+    val companies: List<CompanyListing> = emptyList(),
+    val isLoading: Boolean = false,
+    val isRefreshing: Boolean = false,
+    val searchQuery: String = ""
+)
